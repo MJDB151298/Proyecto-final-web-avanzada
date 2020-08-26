702e383597aa3259c1bfa0ff921589cb7a885b96
Saul Feliciano <jmartinez@intenpro.com>
2020-08-26 19:50:19 -0400

    dockerfiles y charts

--- notifications-microservice/src/main/java/com/proyectofinal/notificationsmicroservice/controllers/NotificationsController.java
+++ notifications-microservice/src/main/java/com/proyectofinal/notificationsmicroservice/controllers/NotificationsController.java
@@ -3,16 +3,28 @@ package com.proyectofinal.notificationsmicroservice.controllers;
 import com.proyectofinal.notificationsmicroservice.entities.Factura;
 import com.proyectofinal.notificationsmicroservice.services.EmailServices;
 import com.proyectofinal.notificationsmicroservice.services.FacturaServices;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.sendgrid.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/factura")
@@ -26,9 +38,24 @@ public class NotificationsController {
     }
 
     @RequestMapping("/createFactura")
-    public String createFactura(String username, String username_email, ArrayList<String> products, double total){
+    public String createFactura(String username, String username_email, ArrayList<String> products, double total, HttpServletResponse response) throws IOException, JRException {
         facturaServices.createFactura(new Factura(username, products, total));
         emailServices.sendEmail(username, username_email, "Compra realizada!", "Aqui el resumen de su compra: ");
+        String sourceFileName = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + "SampleJasperTemplate.jasper").getAbsolutePath();
+        // creating our list of beans
+        List<Factura> dataList = new ArrayList<Factura>();
+        Factura sampleBean = new Factura();
+        sampleBean.setUsername(username);
+        sampleBean.setProducts(products);
+        sampleBean.setTotal(total);
+        dataList.add(sampleBean);
+        // creating datasource from bean list
+        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(dataList);
+        Map parameters = new HashMap();
+        JasperPrint jasperPrint = JasperFillManager.fillReport(sourceFileName, parameters, beanColDataSource);
+        JasperExportManager.exportReportToPdfStream(jasperPrint, response.getOutputStream());
+        response.setContentType("application/pdf");
+        response.addHeader("Content-Disposition", "inline; filename=jasper.pdf;");
         return "compra realizada!";
     }
 
